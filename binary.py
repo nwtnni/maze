5b326e3e655f8b60c3410833ef4b2f078a168d1e
Newton Ni <nwtnni@gmail.com>
2017-10-24 02:14:25 -0400

    Binary tree maze generation algorithm implemented

---
+++ binary.py
@@ -0,0 +1,30 @@
+from sys import argv
+from random import choice
+from maze import *
+
+def generate(w, h):
+    maze = Maze(w, h)
+    
+    # Carving directions
+    hd = choice([D.W, D.E])
+    vd = choice([D.N, D.S])
+    d = [hd, vd]
+
+    # Fill in opposite wall
+    x = 0 if hd == D.W else w - 1
+    y = 0 if vd == D.N else h - 1
+
+    for row in range(h):
+        for col in range(w):
+            p = Point(col, row)
+            
+            if col == x: maze.carve(p, vd)
+            elif row == y: maze.carve(p, hd)
+            else: maze.carve(p, choice(d))
+    return maze
+
+if __name__ == "__main__":
+    if len(argv) != 3:
+        print("Usage: python binary.py <WIDTH> <HEIGHT>")
+    else:
+        print(generate(int(argv[1]), int(argv[2])))
