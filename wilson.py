95cb99ec35fc6ff62660941b14ab3550f6b29c5a
Newton Ni <nwtnni@gmail.com>
2017-10-24 13:42:09 -0400

    Wilson's algorithm

---
+++ wilson.py
@@ -0,0 +1,40 @@
+from maze import *
+from random import choice
+from sys import argv
+
+def prune(path, dirpath):
+    last = path[-1]
+    if path.count(last) > 1:
+        a = path.index(last)
+        path = path[:a + 1]
+        dirpath = dirpath[:a]
+    return (path, dirpath)
+
+def generate(w, h):
+    maze = Maze(w, h)
+    n = maze.rand_point()
+    unknown = set([Point(x, y) for x in range(w) for y in range(h)])
+    unknown.remove(n) 
+
+    while len(unknown) > 0:
+        path = [choice(list(unknown))]
+        dirpath = []
+        
+        while path[-1] in unknown:
+            last = path[-1]
+            dirs = maze.neighbors(last)
+            path.append(last.adj(dirs[0]))
+            dirpath.append(dirs[0]) 
+            path, dirpath = prune(path, dirpath)
+
+        for i in range(len(path) - 1):
+            maze.carve(path[i], dirpath[i])
+            unknown.remove(path[i])
+
+    return maze
+
+if __name__ == "__main__":
+    if len(argv) != 3:
+        print("Usage: python wilson.py <WIDTH> <HEIGHT>")
+    else:
+        print(generate(int(argv[1]), int(argv[2])))
